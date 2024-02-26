12b027cbb8f5a65fd90202975589a85aa5708258
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-02-26 23:34:09 +0100

    Add files via upload

---
+++ springrepositories2/src/main/java/com/springrepositories2/springrepositories2/Springrepositories2Application.java
@@ -0,0 +1,13 @@
+package com.springrepositories2.springrepositories2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrepositories2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springrepositories2Application.class, args);
+	}
+
+}
---
+++ springrepositories2/src/test/java/com/springrepositories2/springrepositories2/Springrepositories2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrepositories2.springrepositories2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springrepositories2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
