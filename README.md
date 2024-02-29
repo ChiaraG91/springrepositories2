6c0e43173d57611b7fc57022614e2ac887051075
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-02-29 10:25:10 +0100

    esercizio caricato

--- README.md
+++
@@ -1,1 +0,0 @@
-# springrepositories2
---
+++ src/main/java/com/springrepositories2/springrepositories2/repositories/ProgrammingLanguageRepository.java
@@ -0,0 +1,10 @@
+package com.springrepositories2.springrepositories2.repositories;
+
+import com.springrepositories2.springrepositories2.entities.ProgrammingLanguage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "repo-prog-languages")
+public interface ProgrammingLanguageRepository extends JpaRepository<ProgrammingLanguage, Long> {
+}
+
